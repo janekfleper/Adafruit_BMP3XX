6fe67c5c3b34fdcc8dec2fc81c4aeed52e94b5e6
Janek Fleper <j.fleper@mailbox.org>
2022-05-27 09:48:34 +0200

    Initialize BMP390 in sleep mode

--- Adafruit_BMP3XX.cpp
+++ Adafruit_BMP3XX.cpp
@@ -234,9 +234,7 @@ bool Adafruit_BMP3XX::_init(void) {
   setPressureOversampling(BMP3_NO_OVERSAMPLING);
   setIIRFilterCoeff(BMP3_IIR_FILTER_DISABLE);
   setOutputDataRate(BMP3_ODR_25_HZ);
-
-  // don't do anything till we request a reading
-  the_sensor.settings.op_mode = BMP3_MODE_FORCED;
+  setPowerMode(BMP3_MODE_SLEEP);
 
   return true;
 }
